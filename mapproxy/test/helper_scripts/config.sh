eda2698f0f498d2cb0d8fe0eaf1a573e71ee03df
Hugo Carr <hugo.carr@flyzipline.com>
2024-05-23 16:39:42 -0700

    redis-cluster: add support
    Add support for redis cluster caching

--- mapproxy/test/helper_scripts/config.sh
+++ mapproxy/test/helper_scripts/config.sh
@@ -2,11 +2,16 @@
 REDIS_CONF="redis.conf"
 REDIS_AUTH_CONF="redis_auth.conf"
 REDIS_TLS_CONF="redis_tls.conf"
+REDIS_CLUSTER_CONF="redis_cluster.conf"
 REDIS_CLI="/usr/bin/redis-cli"
 REDIS_SERVER="/usr/sbin/redis-server"
 REDIS_PORT=6379
 REDIS_TLS_PORT=6380
 REDIS_AUTH_PORT=6381
+REDIS_CLUSTER_PORT=6382
+REDIS_NODE_1_PORT=7000
+REDIS_NODE_2_PORT=7001
+REDIS_NODE_3_PORT=7002
 
 TLS_DIR="../unit/fixture"
 HOSTNAME=$(hostname -f)
